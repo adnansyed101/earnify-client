7206d765b5fda2a5303f05c8b58d2328ff81843a
adnan <sirgeant10@gmail.com>
2025-01-13 19:19:09 +0600

    Add auth provider and toastContainer

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.jsx";
+import AuthProvider from "./provider/AuthProvider.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+     <AuthProvider>
+      <RouterProvider router={router} />
+      <ToastContainer position="top-left" />
+    </AuthProvider>
   </StrictMode>
 );
