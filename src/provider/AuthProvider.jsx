a89f6d650cebfa8821cee52eab310f836d4753a6
adnan <sirgeant10@gmail.com>
2025-01-13 20:13:38 +0600

    Create Context

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,91 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import app from "../config/firebase";
+import { toast } from "react-toastify";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+import AuthContext from "./AuthContext";
+
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+  const [theme, setTheme] = useState("corporate");
+
+  console.log("hello world");
+
+  const toggleTheme = () => {
+    setTheme(theme === "corporate" ? "synthwave" : "corporate");
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const createUserWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const createNewUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const changePassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    toast.warn("Logged out.");
+    return signOut(auth);
+  };
+
+  const updateUserProfile = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  const authInfo = {
+    user,
+    loading,
+    theme,
+    setUser,
+    setLoading,
+    createNewUser,
+    logOut,
+    login,
+    updateUserProfile,
+    createUserWithGoogle,
+    changePassword,
+    toggleTheme,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.array,
+};
+
+export default AuthProvider;
