42c950c94d4af75b8436bf62ceff8b19982df35f
adnan <sirgeant10@gmail.com>
2025-01-14 19:41:41 +0600

    Add dashboard route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home";
+import BuyerHome from "../pages/Dashboard/BuyerHome";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Dashboard from "../layout/Dashboard";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,11 @@ const router = createBrowserRouter([
       { path: "signin", element: <SignIn /> },
       { path: "signup", element: <SignUp /> },
     ],
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [{ index: true, element: <BuyerHome /> }, ],
   },
 ]);
 
