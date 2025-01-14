115f32ab9fb5293c7a39787837458e6031c7c804
adnan <sirgeant10@gmail.com>
2025-01-14 21:47:48 +0600

    Change sidebar links based on role

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -1,23 +1,13 @@
-import { Link } from "react-router-dom";
+import BuyerMenu from "./Menu/BuyerMenu";
+import WorkerMenu from "./Menu/WorkerMenu";
 
 const Sidebar = () => {
+  const role = "Worker";
+
   return (
     <ul className="menu px-4 pt-20">
-      <li className="font-semibold text-lg">
-        <Link to="/dashboard">Home</Link>
-      </li>
-      <li className="font-semibold text-lg">
-        <Link to="/dashboard/addTask">Add New Tasks</Link>
-      </li>
-      <li className="font-semibold text-lg">
-        <Link to="/dashboard/myTasks">My Task&apos;s</Link>
-      </li>
-      <li className="font-semibold text-lg">
-        <Link to="/dashboard/purchaseCoin">Purchase Coin</Link>
-      </li>
-      <li className="font-semibold text-lg">
-        <Link to="/dashboard/paymentHistory">Payment History</Link>
-      </li>
+      {role === "Buyer" && <BuyerMenu />}
+      {role === "Worker" && <WorkerMenu />}
     </ul>
   );
 };
