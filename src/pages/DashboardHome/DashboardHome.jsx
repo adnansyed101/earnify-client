3b07bd1ee83de3f6da9fc54757d46bd598d5dd1f
adnan <sirgeant10@gmail.com>
2025-01-18 23:50:46 +0600

    Show admin home in role is admin

--- src/pages/DashboardHome/DashboardHome.jsx
+++ src/pages/DashboardHome/DashboardHome.jsx
@@ -1,5 +1,6 @@
 import Loading from "../../components/Loading";
 import useGetUser from "../../hooks/useGetUser";
+import AdminHome from "../Dashboard/Admin/AdminHome";
 import BuyerHome from "../Dashboard/Buyer/BuyerHome";
 import WorkerHome from "../Dashboard/Worker/WorkerHome";
 
@@ -12,6 +13,7 @@ const DashboardHome = () => {
     <>
       {userDB?.role === "Buyer" && <BuyerHome />}
       {userDB?.role === "Worker" && <WorkerHome />}
+      {userDB?.role === "Admin" && <AdminHome />}
     </>
   );
 };
