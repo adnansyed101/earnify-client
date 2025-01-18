33fa61b1391a9a06ed0d2dc3bba1f586c5cae68d
adnan <sirgeant10@gmail.com>
2025-01-18 18:55:51 +0600

    Get data from parent

--- src/pages/Dashboard/Buyer/BuyerStats.jsx
+++ src/pages/Dashboard/Buyer/BuyerStats.jsx
@@ -1,36 +1,7 @@
+import PropTypes from "prop-types";
 import { FaBriefcase, FaClock, FaDollarSign } from "react-icons/fa";
-import useAxiosPublic from "../../../hooks/useAxiosPublic";
-import { useQuery } from "@tanstack/react-query";
-import Loading from "../../../components/Loading";
-import useAuth from "../../../hooks/useAuth";
 
-const BuyerStats = () => {
-  const axiosPublic = useAxiosPublic();
-  const { user } = useAuth();
-  const { data: overview = {}, isLoading } = useQuery({
-    queryKey: ["overview"],
-    queryFn: async () => {
-      const { data } = await axiosPublic.get(
-        `/task/overview?email=${user.email}`
-      );
-      return data;
-    },
-  });
-
-  const { data: totalPayments = {}, isLoading: loadingPayments } = useQuery({
-    queryKey: ["totalPayments"],
-    queryFn: async () => {
-      const { data } = await axiosPublic.get(
-        `/payment/totalPayment?email=${user.email}`
-      );
-      return data;
-    },
-  });
-
-  if (isLoading || loadingPayments) {
-    return <Loading />;
-  }
-
+const BuyerStats = ({ totalTasks, pendingTasks, totalPayments }) => {
   return (
     <div className="stats shadow mb-4">
       <div className="stat">
@@ -38,7 +9,7 @@ const BuyerStats = () => {
           <FaBriefcase className="inline-block h-8 w-8 stroke-current" />
         </div>
         <div className="stat-title">Total Tasks</div>
-        <div className="stat-value">{overview.data[0]?.countOfTasks || 0}</div>
+        <div className="stat-value">{totalTasks || 0}</div>
       </div>
 
       <div className="stat">
@@ -46,9 +17,7 @@ const BuyerStats = () => {
           <FaClock className="inline-block h-8 w-8 stroke-current" />
         </div>
         <div className="stat-title">Pending Tasks</div>
-        <div className="stat-value">
-          {overview.data[0]?.totalRequiredWorkers || 0}
-        </div>
+        <div className="stat-value">{pendingTasks || 0}</div>
       </div>
 
       <div className="stat">
@@ -56,10 +25,16 @@ const BuyerStats = () => {
           <FaDollarSign className="inline-block h-8 w-8 stroke-current" />
         </div>
         <div className="stat-title">Total Payments</div>
-        <div className="stat-value">{totalPayments.data[0]?.totalPaid || 0}</div>
+        <div className="stat-value">{totalPayments || 0}</div>
       </div>
     </div>
   );
+};
+
+BuyerStats.propTypes = {
+  totalTasks: PropTypes.number.isRequired,
+  pendingTasks: PropTypes.number.isRequired,
+  totalPayments: PropTypes.number.isRequired,
 };
 
 export default BuyerStats;
