4d678c0d0bf2d75def3b18a48826dac8c4d32d71
adnan <sirgeant10@gmail.com>
2025-01-18 18:18:49 +0600

    Create worker Stats component

---
+++ src/pages/Dashboard/Worker/WorkerStats.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+import { BsCoin } from "react-icons/bs";
+import { FaBriefcase, FaClock } from "react-icons/fa";
+
+const WorkerStats = ({ submissionCount, pendingSubmission, totalEarning }) => {
+  return (
+    <div className="stats shadow mb-4">
+      <div className="stat">
+        <div className="stat-figure text-secondary">
+          <FaBriefcase className="inline-block h-8 w-8 stroke-current" />
+        </div>
+        <div className="stat-title">Submission</div>
+        <div className="stat-value">{submissionCount}</div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-figure text-secondary">
+          <FaClock className="inline-block h-8 w-8 stroke-current" />
+        </div>
+        <div className="stat-title">Pending Submission</div>
+        <div className="stat-value">{pendingSubmission}</div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-figure text-secondary">
+          <BsCoin className="inline-block h-8 w-8 stroke-current" />
+        </div>
+        <div className="stat-title">Earning</div>
+        <div className="stat-value">{totalEarning}</div>
+      </div>
+    </div>
+  );
+};
+
+WorkerStats.propTypes = {
+  submissionCount: PropTypes.number.isRequired,
+  pendingSubmission: PropTypes.number.isRequired,
+  totalEarning: PropTypes.number.isRequired,
+};
+
+export default WorkerStats;
