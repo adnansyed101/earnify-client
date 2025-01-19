bac9e235fdc3b8f9fb651df2aac9668492fabf67
adnan <sirgeant10@gmail.com>
2025-01-19 23:30:01 +0600

    Show Error in title

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Error = () => {
+  useEffect(() => {
+    document.title = "Earnify | 404 Error";
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-10 text-center">
       <h1 className="text-4xl font-semibold">
-        Oops Error 404. Something went wrong. 
+        Oops Error 404. Something went wrong.
       </h1>
       <Link to="/" className="btn btn-primary">
         Go Back To Home
