de26bcf0d5c98c0a8449730d9c6503364d6375c9
adnan <sirgeant10@gmail.com>
2025-01-19 19:12:06 +0600

    Show admin stats from db

--- src/pages/Dashboard/Admin/AdminStats.jsx
+++ src/pages/Dashboard/Admin/AdminStats.jsx
@@ -1,8 +1,14 @@
 import { FaBriefcase, FaDollarSign } from "react-icons/fa";
 import { GrUserWorker } from "react-icons/gr";
 import { BsCoin } from "react-icons/bs";
+import PropTypes from "prop-types";
 
-const AdminStats = () => {
+const AdminStats = ({
+  totalWorker,
+  totalBuyer,
+  totalAvailableCoin,
+  totalPayment,
+}) => {
   return (
     <div className="stats shadow mb-4">
       <div className="stat">
@@ -10,7 +16,7 @@ const AdminStats = () => {
           <FaBriefcase className="inline-block h-8 w-8 stroke-current" />
         </div>
         <div className="stat-title">Total Worker</div>
-        <div className="stat-value">{0}</div>
+        <div className="stat-value">{totalWorker}</div>
       </div>
 
       <div className="stat">
@@ -18,7 +24,7 @@ const AdminStats = () => {
           <GrUserWorker className="inline-block h-8 w-8 stroke-current" />
         </div>
         <div className="stat-title">Total Buyer</div>
-        <div className="stat-value">{0}</div>
+        <div className="stat-value">{totalBuyer}</div>
       </div>
 
       <div className="stat">
@@ -26,7 +32,7 @@ const AdminStats = () => {
           <BsCoin className="inline-block h-8 w-8 stroke-current" />
         </div>
         <div className="stat-title">Total Available Coin</div>
-        <div className="stat-value">{0}</div>
+        <div className="stat-value">{totalAvailableCoin}</div>
       </div>
 
       <div className="stat">
@@ -34,10 +40,17 @@ const AdminStats = () => {
           <FaDollarSign className="inline-block h-8 w-8 stroke-current" />
         </div>
         <div className="stat-title">Total Payments</div>
-        <div className="stat-value">{0}</div>
+        <div className="stat-value">{totalPayment}</div>
       </div>
     </div>
   );
+};
+
+AdminStats.propTypes = {
+  totalWorker: PropTypes.number.isRequired,
+  totalBuyer: PropTypes.number.isRequired,
+  totalAvailableCoin: PropTypes.number.isRequired,
+  totalPayment: PropTypes.number.isRequired,
 };
 
 export default AdminStats;
