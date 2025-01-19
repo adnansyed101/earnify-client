4186cbc5863567309f4ec67ae9c744d13a9c8f7e
adnan <sirgeant10@gmail.com>
2025-01-19 19:11:50 +0600

    Handle approve and reject

--- src/pages/Dashboard/Admin/AdminHome.jsx
+++ src/pages/Dashboard/Admin/AdminHome.jsx
@@ -1,10 +1,73 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import AdminStats from "./AdminStats";
+import Loading from "../../../components/Loading";
+import { toast } from "react-toastify";
 
 const AdminHome = () => {
+  const axiosPublic = useAxiosPublic();
+
+  const {
+    data: adminOverview = {},
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["adminOverview"],
+    queryFn: async () => {
+      const { data } = await axiosPublic.get("/overview/admin");
+      return data;
+    },
+  });
+
+  const handleApprove = async (
+    workerID,
+    workerCoin,
+    withdrawalCoin,
+    withdrawalID
+  ) => {
+    try {
+      await axiosPublic.patch(`/user/updatecoin/${workerID}`, {
+        coin: workerCoin - withdrawalCoin,
+      });
+      await axiosPublic.patch(`/withdrawal/status/${withdrawalID}`, {
+        status: "accepted",
+      });
+      refetch();
+      toast.success("Accepted Withdrawal.");
+    } catch (err) {
+      toast.err(err.message);
+    }
+  };
+
+  const handleReject = async (withdrawalID) => {
+    try {
+      await axiosPublic.patch(`/withdrawal/status/${withdrawalID}`, {
+        status: "rejected",
+      });
+      refetch();
+      toast.success("Withdrawal Rejected");
+    } catch (err) {
+      toast.err(err.message);
+    }
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  console.log(adminOverview);
+
   return (
     <div className="mt-12">
       <h1 className="text-2xl font-bold mb-2">Overview</h1>
-      <AdminStats />
+      <AdminStats
+        totalWorker={adminOverview.data?.totalWorker || 0}
+        totalBuyer={adminOverview.data?.totalBuyer || 0}
+        totalAvailableCoin={
+          adminOverview.data.totalAvailableCoin[0]?.totalCoins || 0
+        }
+        totalPayment={adminOverview.data.totalPayment[0]?.totalPaid || 0}
+      />
       <section className="bg-gray-100 py-10">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">
@@ -21,49 +84,43 @@ const AdminHome = () => {
                 </tr>
               </thead>
               <tbody>
-                {/* {submissions.data.submissions.map((submission) => (
-                  <tr key={submission._id} className="hover:bg-gray-100">
-                    <td className="p-4">{submission.worker.name}</td>
-                    <td className="p-4">{submission.task.title}</td>
-                    <td className="p-4">{submission.task.payableAmount}</td>
+                {adminOverview.data.pendingPayments.map((payment) => (
+                  <tr key={payment._id} className="hover:bg-gray-100">
+                    <td className="p-4">{payment.worker.name}</td>
+                    <td className="p-4">{payment.withdrawalCoin}</td>
+                    <td className="p-4">$ {payment.withdrawalAmount}</td>
 
                     <td className="p-4 space-x-2">
-                      {submission.status === "pending" ? (
+                      {payment.status === "pending" ? (
                         <>
                           <button
+                            className="btn btn-success btn-sm"
                             onClick={() =>
                               handleApprove(
-                                submission._id,
-                                submission.worker._id,
-                                submission.worker.coin,
-                                submission.task.payableAmount
+                                payment.worker._id,
+                                payment.worker.coin,
+                                payment.withdrawalCoin,
+                                payment._id
                               )
                             }
-                            className="btn btn-success btn-sm"
                           >
                             Approve
                           </button>
                           <button
-                            onClick={() =>
-                              handleReject(
-                                submission._id,
-                                submission.task._id,
-                                submission.task.requiredWorkers
-                              )
-                            }
+                            onClick={() => handleReject(payment._id)}
                             className="btn btn-error btn-sm"
                           >
                             Reject
                           </button>
                         </>
-                      ) : submission.status === "accepted" ? (
+                      ) : payment.status === "accepted" ? (
                         <div className="badge badge-success">Accepted</div>
                       ) : (
                         <div className="badge badge-error">Rejected</div>
                       )}
                     </td>
                   </tr>
-                ))} */}
+                ))}
               </tbody>
             </table>
           </div>
