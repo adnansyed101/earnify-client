48cd9e6db86e82c131c2960645f63c0abf2755a7
adnan <sirgeant10@gmail.com>
2025-01-20 21:11:01 +0600

    Show best workers from db

--- src/components/Home/BestWorkerSection.jsx
+++ src/components/Home/BestWorkerSection.jsx
@@ -1,59 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
 import { BsCoin } from "react-icons/bs";
-
-const workers = [
-  {
-    id: 1,
-    name: "Alice Johnson",
-    coins: 1500,
-    image:
-      "https://img.freepik.com/free-photo/photo-cheerful-curly-haired-woman-feels-like-winner-clenches-fists-makes-victory-gesture-exclaims-with-happiness-wears-yellow-jumper-achieves-goal-gets-triumph-poses-indoor-yes-i-did-it_273609-37571.jpg?semt=ais_hybrid",
-  },
-  {
-    id: 2,
-    name: "Bob Smith",
-    coins: 1200,
-    image:
-      "https://img.freepik.com/free-photo/photo-cheerful-curly-haired-woman-feels-like-winner-clenches-fists-makes-victory-gesture-exclaims-with-happiness-wears-yellow-jumper-achieves-goal-gets-triumph-poses-indoor-yes-i-did-it_273609-37571.jpg?semt=ais_hybrid",
-  },
-  {
-    id: 3,
-    name: "Catherine Brown",
-    coins: 1100,
-    image:
-      "https://img.freepik.com/free-photo/photo-cheerful-curly-haired-woman-feels-like-winner-clenches-fists-makes-victory-gesture-exclaims-with-happiness-wears-yellow-jumper-achieves-goal-gets-triumph-poses-indoor-yes-i-did-it_273609-37571.jpg?semt=ais_hybrid",
-  },
-  {
-    id: 4,
-    name: "David Wilson",
-    coins: 1050,
-    image:
-      "https://img.freepik.com/free-photo/photo-cheerful-curly-haired-woman-feels-like-winner-clenches-fists-makes-victory-gesture-exclaims-with-happiness-wears-yellow-jumper-achieves-goal-gets-triumph-poses-indoor-yes-i-did-it_273609-37571.jpg?semt=ais_hybrid",
-  },
-  {
-    id: 5,
-    name: "Emily Davis",
-    coins: 980,
-    image:
-      "https://img.freepik.com/free-photo/photo-cheerful-curly-haired-woman-feels-like-winner-clenches-fists-makes-victory-gesture-exclaims-with-happiness-wears-yellow-jumper-achieves-goal-gets-triumph-poses-indoor-yes-i-did-it_273609-37571.jpg?semt=ais_hybrid",
-  },
-  {
-    id: 6,
-    name: "Franklin Harris",
-    coins: 900,
-    image:
-      "https://img.freepik.com/free-photo/photo-cheerful-curly-haired-woman-feels-like-winner-clenches-fists-makes-victory-gesture-exclaims-with-happiness-wears-yellow-jumper-achieves-goal-gets-triumph-poses-indoor-yes-i-did-it_273609-37571.jpg?semt=ais_hybrid",
-  },
-];
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import Loading from "../Loading";
 
 const BestWorkerSection = () => {
+  const axiosPublic = useAxiosPublic();
+  const { data: bestWorkers = {}, isLoading } = useQuery({
+    queryKey: ["bestWorkers"],
+    queryFn: async () => {
+      const { data } = await axiosPublic.get(`/user/bestworker`);
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <section className="py-10">
       <div className="container mx-auto px-2">
         <h2 className="text-3xl font-bold text-center mb-8">Best Workers</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4">
-          {workers.map((worker) => (
+          {bestWorkers.data.map((worker) => (
             <div
-              key={worker.id}
+              key={worker._id}
               className="card bg-white shadow-lg p-6 rounded-lg flex flex-col items-center"
             >
               <div className="avatar">
@@ -63,7 +34,7 @@ const BestWorkerSection = () => {
               </div>
               <h3 className="text-lg font-semibold mt-4">{worker.name}</h3>
               <p className="flex gap-1 items-center">
-                {worker.coins} <BsCoin />
+                {worker.coin} <BsCoin />
               </p>
             </div>
           ))}
