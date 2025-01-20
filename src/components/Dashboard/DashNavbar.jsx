d02e639a8cdfde9856390b306a488a420ee1aa1c
adnan <sirgeant10@gmail.com>
2025-01-20 14:24:08 +0600

    Rename navbar to dashNavbar

---
+++ src/components/Dashboard/DashNavbar.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import { FaBars, FaBell } from "react-icons/fa";
+import { useEffect } from "react";
+import useAuth from "../../hooks/useAuth";
+import { BsCoin } from "react-icons/bs";
+import NavbarButtons from "../NavbarButtons";
+import BuyerMenu from "./Menu/BuyerMenu";
+import WorkerMenu from "./Menu/WorkerMenu";
+import AdminMenu from "./Menu/AdminMenu";
+import useGetUser from "../../hooks/useGetUser";
+
+const DashNavbar = () => {
+  const { theme } = useAuth();
+  const { userDB } = useGetUser();
+  const role = userDB?.role;
+
+  useEffect(() => {
+    document.querySelector("html").setAttribute("data-theme", theme);
+  }, [theme]);
+
+  return (
+    <header>
+      <div className="navbar justify-between w-full fixed top-0 bg-secondary z-50">
+        <div className="navbar-start">
+          <Link
+            to="/"
+            className="btn btn-ghost rounded-full px-2 md:px-4 md:text-xl"
+          >
+            <BsCoin />
+            Earnify
+          </Link>
+        </div>
+        <div className="navbar-end space-x-1">
+          <NavbarButtons />
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <FaBell className="h-5 w-5" />
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+          <div className="dropdown dropdown-end lg:hidden">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <FaBars />
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {role === "Buyer" && <BuyerMenu />}
+              {role === "Worker" && <WorkerMenu />}
+              {role === "Admin" && <AdminMenu />}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default DashNavbar;
