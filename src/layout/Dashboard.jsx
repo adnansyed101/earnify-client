320d306f7139dde523dffb4e5a2322f5416b93d2
Adnan <sirgeant10@gmail.com>
2025-02-01 14:55:01 +0600

    Add validation for email.
    If the user's email is not verified the user cannot go to the dashboard
    page.

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,14 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import DashNavbar from "../components/Dashboard/DashNavbar";
 import Sidebar from "../components/Dashboard/Sidebar";
 import Footer from "../components/Home/Footer";
 import { useEffect } from "react";
+import useAuth from "../hooks/useAuth";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
+  const { user } = useAuth();
+
   useEffect(() => {
     document.title = "Earnify | Dashboard Home";
     window.scrollTo(0, 0);
   }, []);
+
+  if (!user?.emailVerified) {
+    console.log("Hello world");
+
+    toast.warn("Please verify your email.", {
+      toastId: "verifyEmail",
+    });
+    return <Navigate to={"/"} />;
+  }
 
   return (
     <>
