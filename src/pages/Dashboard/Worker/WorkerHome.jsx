a779b61385ba3cee98a5565dce1cb60723129359
Adnan <sirgeant10@gmail.com>
2025-02-18 22:09:07 +0600

    Remove unnecessary styles.

--- src/pages/Dashboard/Worker/WorkerHome.jsx
+++ src/pages/Dashboard/Worker/WorkerHome.jsx
@@ -23,52 +23,52 @@ const WorkerHome = () => {
   }
 
   return (
-    <div className="mt-16">
+    <div>
       <h1 className="text-2xl font-bold mb-2">Overview</h1>
       <Stats
         submissionCount={submissions.data?.submissionCount || 0}
         pendingSubmission={submissions.data?.pendingSubmissionsCount || 0}
         totalEarning={submissions.data.totalEarning?.totalPayableAmount || 0}
       />
-      <section className="py-10">
-        <div className="container mx-auto">
-          <h2 className="text-3xl font-bold text-center mb-8">
-            Approved Submissions
-          </h2>
-          <div className="overflow-x-auto">
-            <table className="table w-full bg-base-200 shadow-md rounded-lg whitespace-nowrap">
-              <thead>
-                <tr>
-                  <th className="p-2 md:p-4">Task Title</th>
-                  <th className="p-2 md:p-4">Payable Amount</th>
-                  <th className="p-2 md:p-4">Buyer Name</th>
-                  <th className="p-2 md:p-4">Status</th>
+      <section>
+        <h2 className="text-3xl font-bold text-center mb-8">
+          Approved Submissions
+        </h2>
+        <div className="overflow-x-auto">
+          <table className="table w-full bg-base-200 shadow-md rounded-lg whitespace-nowrap">
+            <thead>
+              <tr>
+                <th className="p-2 md:p-4">Task Title</th>
+                <th className="p-2 md:p-4">Payable Amount</th>
+                <th className="p-2 md:p-4">Buyer Name</th>
+                <th className="p-2 md:p-4">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {submissions.data.acceptedSubmissions.map((submission) => (
+                <tr key={submission._id} className="hover:bg-base-300">
+                  <td className="p-2 md:p-4">{submission.task.title}</td>
+                  <td className="p-2 md:p-4">
+                    {submission.task.payableAmount}
+                  </td>
+                  <td className="p-2 md:p-4">{submission.buyer.name}</td>
+                  <td className="p-2 md:p-4 space-x-2">
+                    <span
+                      className={`badge ${
+                        submission.status === "accepted"
+                          ? "badge-success"
+                          : submission.status === "pending"
+                          ? "badge-warning"
+                          : "badge-error"
+                      }`}
+                    >
+                      {submission.status}
+                    </span>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {submissions.data.acceptedSubmissions.map((submission) => (
-                  <tr key={submission._id} className="hover:bg-base-300">
-                    <td className="p-2 md:p-4">{submission.task.title}</td>
-                    <td className="p-2 md:p-4">{submission.task.payableAmount}</td>
-                    <td className="p-2 md:p-4">{submission.buyer.name}</td>
-                    <td className="p-2 md:p-4 space-x-2">
-                      <span
-                        className={`badge ${
-                          submission.status === "accepted"
-                            ? "badge-success"
-                            : submission.status === "pending"
-                            ? "badge-warning"
-                            : "badge-error"
-                        }`}
-                      >
-                        {submission.status}
-                      </span>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
       </section>
     </div>
