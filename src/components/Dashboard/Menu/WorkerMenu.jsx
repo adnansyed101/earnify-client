31c9f0c54af85a86f73fc93d56d7bf08393b9918
Adnan <sirgeant10@gmail.com>
2025-02-18 22:41:24 +0600

    Remove div.

--- src/components/Dashboard/Menu/WorkerMenu.jsx
+++ src/components/Dashboard/Menu/WorkerMenu.jsx
@@ -2,23 +2,20 @@ import { Link } from "react-router-dom";
 
 const WorkerMenu = () => {
   return (
-    <div>
-      <>
-        <li className="font-semibold text-lg">
-          <Link to="/dashboard">Home</Link>
-        </li>
-        <li className="font-semibold text-lg">
-          <Link to="/dashboard/tasklist">Task List</Link>
-        </li>
-        <li className="font-semibold text-lg">
-          <Link to="/dashboard/mysubmission">My Submission</Link>
-        </li>
-        <li className="font-semibold text-lg">
-          <Link to="/dashboard/withdrawals">Withdrawals</Link>
-        </li>
-        
-      </>
-    </div>
+    <>
+      <li className="font-semibold text-lg">
+        <Link to="/dashboard">Home</Link>
+      </li>
+      <li className="font-semibold text-lg">
+        <Link to="/dashboard/tasklist">Task List</Link>
+      </li>
+      <li className="font-semibold text-lg">
+        <Link to="/dashboard/mysubmission">My Submission</Link>
+      </li>
+      <li className="font-semibold text-lg">
+        <Link to="/dashboard/withdrawals">Withdrawals</Link>
+      </li>
+    </>
   );
 };
 
