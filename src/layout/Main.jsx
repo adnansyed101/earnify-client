2649143b4fb3d5688d14f8938513a7a86a16124d
Adnan <sirgeant10@gmail.com>
2025-02-18 22:27:44 +0600

    Fix footer import route

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Home/Navbar";
-import Footer from "../components/Home/Footer";
+import Footer from "../components/Footer";
 
 const Main = () => {
   return (
