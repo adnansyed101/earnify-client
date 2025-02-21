295b7f654aa436e83a29755ed56cfb631077a8ec
Adnan <sirgeant10@gmail.com>
2025-02-21 15:14:16 +0600

    Move CAT up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,8 +25,8 @@ const Home = () => {
       <SuccessStories />
       <LivePlatformStats />
       <Pricing />
-      <TestimonialSection />
       <CallToAction />
+      <TestimonialSection />
       <Newsletter />
     </>
   );
