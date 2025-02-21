e480a88ebca6439fb08b67a9684fcae31f197006
Adnan <sirgeant10@gmail.com>
2025-02-21 14:45:30 +0600

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,29 @@ export default {
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["acid", "synthwave"],
+    themes: [
+      {
+        light: {
+          primary: "#4CAF50",
+          secondary: "#FFC107",
+          accent: "#2196F3",
+          neutral: "#E0E0E0",
+          "base-100": "#F9FAFB",
+          success: "#34D399",
+          error: "#EF4444",
+        },
+      },
+      {
+        dark: {
+          primary: "#34D399",
+          secondary: "#FACC15",
+          accent: "#3B82F6",
+          neutral: "#374151",
+          "base-100": "#1F2937",
+          success: "#10B981",
+          error: "#F87171",
+        },
+      },
+    ],
   },
 };
