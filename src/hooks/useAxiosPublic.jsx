f6d76cbb6d04a6167b9c49132021bd3f8d515829
adnan <sirgeant10@gmail.com>
2025-03-21 18:22:06 +0600

    Firebase files.

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 // Server Link -- https://earnify-snowy.vercel.app
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://earnify-snowy.vercel.app",
 });
 
 const useAxiosPublic = () => {
